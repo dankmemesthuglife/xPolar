4cc2f33b7f4f3ef837706bced28b7049b24dbbf4
Juan Boschero <juan.boschero@hotmail.com>
2017-07-28 20:04:34 -0300

    Its the same thing but overtime it sucks ass, it changes to Java

---
+++ src/InitialArr.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Array;
+
+/**
+ * Created by juanboschero on 7/17/17.
+ */
+public class InitialArr {
+
+    public static void main(String[] args){
+        final String dir = System.getProperty("user.dir");
+        System.out.println("current dir = " + dir);
+    }
+}
+
